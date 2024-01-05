16f5d82fd136cfedf5cdfa83131afdb76735f877
BiliOrange <90908765+orangeZSCB@users.noreply.github.com>
2024-01-05 21:48:11 +0800

    Update tgkt.py
    updated something.

--- src_py/tgkt.py
+++ src_py/tgkt.py
@@ -2,11 +2,24 @@ import random
 import json
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-
+import sys
 app = Flask(__name__)
 CORS(app)
 
-with open("/home/orange/TangokotoAPI/Tangokoto.txt", "r", encoding="utf-8") as file: # please change the folder!!!!!!
+def htmlspecialchars(text):
+    return (
+        text.replace("&", "&amp;").
+        replace('"', "&quot;").
+        replace("<", "&lt;").
+        replace(">", "&gt;")
+    )
+
+if len(sys.argv) > 1:
+    file_path = sys.argv[1]
+else:
+    file_path = "/home/orange/TangokotoAPI/Tangokoto.txt"  # path
+
+with open(file_path, "r", encoding="utf-8") as file:
     tangokoto = file.readlines()
 
 author = "唐老师"
@@ -16,7 +29,8 @@ length = len(tangokoto)
 def get_tangokoto():
     randid = random.randint(0, length - 1)
     index = int(request.args.get('id', randid))
-    index = min(max(index, 0), length - 1)  # Ensure index is within range
+
+    index = randid if index < 0 or index >= length else index
 
     output_raw = tangokoto[index].rstrip()
     output = {
@@ -30,14 +44,16 @@ def get_tangokoto():
     if output_type == 'plain':
         return output_raw, 200, {'Content-Type': 'text/plain; charset=utf-8'}
     elif output_type == 'js':
-        js_output = f"function doTangokoto(){{document.write(\"<span class='hitokoto' title='Tangokoto'>{output_raw}</span>\");}}"
+        js_output = f"function doTangokoto(){{document.write(\"<span class='hitokoto' title='Tangokoto'>{htmlspecialchars(output_raw)}</span>\");}}"
         return js_output, 200, {'Content-Type': 'application/javascript; charset=utf-8'}
     elif output_type == 'jsonp':
         callback = request.args.get('_callback', 'doTangokoto')
-        jsonp_output = f"{callback}({json.dumps(output)})"  # JSONP output
+        jsonp_output = f"{callback}({json.dumps({k: htmlspecialchars(v) for k, v in output.items()})})"  # JSONP output
         return jsonp_output, 200, {'Content-Type': 'application/javascript; charset=utf-8'}
     else:
         return jsonify(output), 200, {'Content-Type': 'application/json; charset=utf-8'}
 
 if __name__ == '__main__':
     app.run()
+
+# edited: file path, api path, xss defend, id.
